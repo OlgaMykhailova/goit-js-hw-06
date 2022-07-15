21af4e041114251a5ec2f1f5e8930841879b23ef
OlgaMykhailova <rosebud_z@yahoo.com>
2022-07-15 22:41:13 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesEl = document.querySelector('#categories').children;
+
+console.log(`Number of categories: ${categoriesEl.length}`);
+
+for (let i = 0; i < categoriesEl.length; i += 1) {
+    console.log(`Category: ${categoriesEl[i].firstElementChild.textContent}`);
+    console.log(`Elements: ${categoriesEl[i].lastElementChild.children.length}`);
+}
